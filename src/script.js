536ef660f69df7b887f977184d175aa95b6eb972
Light Andy <andy.197197@yahoo.com>
2023-11-06 15:06:15 +0200

    Added all the logic needed

--- src/script.js
+++ src/script.js
@@ -1,17 +1,99 @@
 document.querySelectorAll(".box").forEach((box) => {
   let clickCount = 0;
-  const originalColor = box.style.backgroundColor;
 
   box.addEventListener("click", () => {
     clickCount++;
 
     if (clickCount === 1) {
-      box.style.backgroundColor = "#69dda7";
+      box.classList.remove("off");
+      box.classList.add("on");
     } else if (clickCount === 2) {
-      box.style.backgroundColor = "#ca7d80";
-    } else {
-      box.style.backgroundColor = originalColor;
+      box.classList.remove("on");
+      box.classList.add("off");
       clickCount = 0;
+    } else {
+      box.classList.remove("on", "off");
+      clickCount = 1;
     }
   });
 });
+
+function handleSave() {
+  createStatusJSON();
+}
+
+function createStatusJSON() {
+  const boxes = [];
+
+  const boxElements = document.getElementsByClassName("box");
+  for (let i = 0; i < boxElements.length; i++) {
+    const boxElement = boxElements[i];
+    let status = "disabled";
+
+    if (boxElement.classList.contains("on")) {
+      status = "on";
+    } else if (boxElement.classList.contains("off")) {
+      status = "off";
+    }
+
+    const box = {
+      id: i + 1,
+      status: status,
+    };
+    boxes.push(box);
+  }
+
+  const jsonString = JSON.stringify(boxes);
+
+  const blob = new Blob([jsonString], { type: "application/json" });
+
+  saveAs(blob, "status.json");
+}
+
+function handleImport() {
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+
+  fileInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const jsonContent = e.target.result;
+      const boxes = JSON.parse(jsonContent);
+
+      updateBoxClasses(boxes);
+    };
+
+    reader.readAsText(file);
+  });
+
+  fileInput.click();
+}
+
+function updateBoxClasses(boxes) {
+  const boxElements = document.getElementsByClassName("box");
+  for (let i = 0; i < boxElements.length; i++) {
+    const boxElement = boxElements[i];
+    const box = boxes[i];
+
+    boxElement.classList.remove("on", "off", "disabled");
+
+    if (box.status === "on") {
+      boxElement.classList.add("on");
+    } else if (box.status === "off") {
+      boxElement.classList.add("off");
+    } else if (box.status === "disabled") {
+      boxElement.classList.add("disabled");
+    }
+  }
+}
+
+function handleReset() {
+  const boxElements = document.getElementsByClassName("box");
+  for (let i = 0; i < boxElements.length; i++) {
+    const boxElement = boxElements[i];
+    boxElement.classList.remove("on", "off");
+    boxElement.removeAttribute("style");
+  }
+}
